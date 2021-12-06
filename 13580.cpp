60dde9ce202874132754ea5e0ba09bdb7ebd98bc
yunjikim-31 <kwg531@naver.com>
2021-12-06 10:00:04 +0900

    Andando no tempo

---
+++ 13580.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a[3];
+
+int main()
+{
+    for (int i = 0; i < 3; i++)
+        cin >> a[i];
+
+    sort(a, a + 3);
+
+    if (a[0] == a[1] || a[1] == a[2] || a[0] + a[1] == a[2])
+        cout << "S";
+    else
+        cout << "N";
+
+    return 0;
+}
